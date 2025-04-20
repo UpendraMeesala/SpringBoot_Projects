c451a399b6973d149f98839c209e466302303f69
upendra <upendrameesala.com>
2025-04-20 18:25:44 +0530

    first commit

---
+++ Currency-service-exchange/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(10,10001,'USD','INR','');
+
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(20,10002,'EUR','INR','');
+
+insert into currency_exchange
+(conversion_multiple,id,currency_from,currency_to,environment)
+values(30,10003,'AUR','INR','');
+
